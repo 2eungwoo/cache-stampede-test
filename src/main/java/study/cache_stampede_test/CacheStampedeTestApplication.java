84a436eff2a2d57194039b4be8c31b7b9a25a16e
swlee <swo98@naver.com>
2025-10-15 23:34:10 +0900

    init: project init

---
+++ src/main/java/study/cache_stampede_test/CacheStampedeTestApplication.java
@@ -0,0 +1,13 @@
+package study.cache_stampede_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStampedeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheStampedeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/cache_stampede_test/CacheStampedeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.cache_stampede_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStampedeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
