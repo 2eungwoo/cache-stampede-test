8c164ee606590f88089c8d9534cac24d37c3dae6
swlee <swo98@naver.com>
2025-10-17 20:06:27 +0900

    add: redis메트릭, 스프링부트 메트릭 모니터링 구축

---
+++ script.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 100, // 100명의 가상 유저
+  duration: '30s', // 30초 동안 테스트 수행
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080/api/members');
+  check(res, { 'status was 200': (r) => r.status === 200 });
+  sleep(1);
+}
